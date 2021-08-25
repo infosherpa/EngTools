9026377b74669d932ce13334085f9ba157df93c7
Liam <lmchale@protonmail.com>
2021-08-24 21:52:42 -0400

    debug regex

--- heroku_configuration.py
+++ heroku_configuration.py
@@ -16,9 +16,9 @@ with open('tunnelworks/settings.py', 'r+') as f:
     if line.startswith("INSTALLED_APPS = ["):
       line = line + "    'whitenoise.runserver_nostatic'," + '\n'
     
-    re.sub('DEBUG = False', 'DEBUG = True', line)
-    re.sub('ALLOWED_HOSTS = \[]', "ALLOWED_HOSTS = \['127\.0\.0\.1', 'engtools\.herokuapp\.com'\]", line)
-    re.sub('django\.db\.backends\.mysql', 'django\.db\.backends\.postgresql_psycopg2', line)
+    line = re.sub('DEBUG = False', 'DEBUG = True', line)
+    line = re.sub(r'ALLOWED_HOSTS = \[]', r"ALLOWED_HOSTS = \['127\.0\.0\.1', 'engtools\.herokuapp\.com'\]", line)
+    line = re.sub(r'django\.db\.backends\.mysql', r'django\.db\.backends\.postgresql_psycopg2', line)
       
     temp.write(line)
   f.close()
@@ -34,12 +34,13 @@ temp.write(database_sett)
 
 django_heroku = "django_heroku.settings(locals())"+'\n'
 temp.write(django_heroku)
+temp.write('\n')
 
 temp.close()
 shutil.move('temp', 'tunnelworks/settings.py')
 
 
-with open('requirements,txt', 'a') as f:
+with open('requirements.txt', 'a') as f:
   f.write('psycopg2-binary==2.9.1'+'\n')
   f.write('gunicorn==20.1.0'+'\n')
   f.write('dj-database-url==0.5.0'+'\n')
