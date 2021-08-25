b4975f1af0a88c2153f14ce118029fb061b392f7
Liam <lmchale@protonmail.com>
2021-08-24 22:10:06 -0400

    another fix

--- heroku_configuration.py
+++ heroku_configuration.py
@@ -16,9 +16,9 @@ with open('tunnelworks/settings.py', 'r+') as f:
     if line.startswith("INSTALLED_APPS = ["):
       line = line + "    'whitenoise.runserver_nostatic'," + '\n'
     
-    line = re.sub('DEBUG = False', 'DEBUG = True', line)
-    line = re.sub(r'ALLOWED_HOSTS = \[]', r"ALLOWED_HOSTS = \['127\.0\.0\.1', 'engtools\.herokuapp\.com'\]", line)
-    line = re.sub(r'django\.db\.backends\.mysql', r'django\.db\.backends\.postgresql_psycopg2', line)
+    line = re.sub('DEBUG = True', 'DEBUG = False', line)
+    line = re.sub(r'ALLOWED_HOSTS = \[]', r"ALLOWED_HOSTS = ['127.0.0.1', 'engtools.herokuapp.com']", line)
+    line = re.sub(r'django\.db\.backends\.mysql', r'django.db.backends.postgresql_psycopg2', line)
       
     temp.write(line)
   f.close()
@@ -28,9 +28,11 @@ temp.write(whitenoise_finders)
 
 storage = "STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'" + '\n'
 temp.write(storage)
+temp.write('\n')
 
 database_sett = "DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)"+'\n'
 temp.write(database_sett)
+temp.write('\n')
 
 django_heroku = "django_heroku.settings(locals())"+'\n'
 temp.write(django_heroku)
@@ -47,4 +49,5 @@ with open('requirements.txt', 'a') as f:
   f.write('django-heroku==0.3.1'+'\n')
   f.write('whitenoise==5.3.0'+'\n')
   f.write('psycopg2==2.9.1'+'\n')
+  f.write(''+'\n')
   f.close()
